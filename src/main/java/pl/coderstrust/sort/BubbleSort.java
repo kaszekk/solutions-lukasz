4c8c4536278b1a9ba7a4650541740cd56fde0d2e
Lukasz <lukmyrta@gmail.com>
2018-11-08 00:50:49 +0100

    Selection sort working.may need optimalisation.

--- src/main/java/pl/coderstrust/sort/BubbleSort.java
+++ src/main/java/pl/coderstrust/sort/BubbleSort.java
@@ -1,4 +1,13 @@
 package pl.coderstrust.sort;
 
 public class BubbleSort {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    int[] sort(int[] array) {
+
+        return array;
+    }
+
 }
--- src/main/java/pl/coderstrust/sort/SelectionSort.java
+++ src/main/java/pl/coderstrust/sort/SelectionSort.java
@@ -1,4 +1,30 @@
 package pl.coderstrust.sort;
 
 public class SelectionSort {
+    public static void main(String[] args) {
+        int[] array = {-7, 6, 7, 2, 3, 1, 0, 5, 12, -4, 9, 8};
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+        sort(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    public static int[] sort(int[] array) {
+        for (int j = 0; j < array.length; j++) {
+            int minIndex = j;
+            for (int i = j; i < array.length - 1; i++) {
+                if (array[i + 1] < array[minIndex]) {
+                    minIndex = i + 1;
+                }
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[j];
+            array[j] = temp;
+        }
+        return array;
+    }
 }
