bed40fb29bcd4dedb5d63c51fc841efae872df9f
Lukasz <lukmyrta@gmail.com>
2018-11-07 19:18:16 +0100

    Basic structure added

---
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciIterative.java
@@ -0,0 +1,7 @@
+package pl.coderstrust.fibonacci;
+
+public class FibonacciIterative {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciRecursive.java
@@ -0,0 +1,7 @@
+package pl.coderstrust.fibonacci;
+
+public class FibonacciRecursive {
+    public static void main(String[] args) {
+
+    }
+}
