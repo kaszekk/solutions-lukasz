08fef644bf0f85f337bbc5b6aefac7c08bc7bb63
Lukasz <lukmyrta@gmail.com>
2018-11-07 23:21:38 +0100

    both implementations tested.OK

--- src/main/java/pl/coderstrust/fibonacci/FibonacciRecursive.java
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciRecursive.java
@@ -1,11 +1,11 @@
 package pl.coderstrust.fibonacci;
 
 public class FibonacciRecursive {
-    public static void main(String[] args) {
-
-    }
-
     public static long fibonacci(int fibonacciNumberInOrder) {
-        return 4L;
+        if (fibonacciNumberInOrder == 1 || fibonacciNumberInOrder == 2) {
+            return 1L;
+        } else {
+            return fibonacci(fibonacciNumberInOrder - 1) + fibonacci(fibonacciNumberInOrder - 2);
+        }
     }
 }
