5f4edf40dceacd31362544207913d41f45464360
Lukasz <lukmyrta@gmail.com>
2018-11-09 00:58:05 +0100

    arrayOfInt parameter renamed to array

--- src/main/java/pl/coderstrust/sort/BubbleSort.java
+++ src/main/java/pl/coderstrust/sort/BubbleSort.java
@@ -1,15 +1,15 @@
 package pl.coderstrust.sort;
 
 public class BubbleSort {
-    public static int[] sort(int[] arrayOfInt) {
-        int[] copyOfArrayOfInt = new int[arrayOfInt.length];
-        for (int i = 0; i < arrayOfInt.length; i++) {
-            copyOfArrayOfInt[i] = arrayOfInt[i];
+    public static int[] sort(int[] array) {
+        int[] copyOfArrayOfInt = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            copyOfArrayOfInt[i] = array[i];
         }
         boolean wasSwapped = true;
         while (wasSwapped) {
             wasSwapped = false;
-            for (int i = 0; i < arrayOfInt.length - 1; i++) {
+            for (int i = 0; i < array.length - 1; i++) {
                 if (copyOfArrayOfInt[i + 1] < copyOfArrayOfInt[i]) {
                     int temp = copyOfArrayOfInt[i];
                     copyOfArrayOfInt[i] = copyOfArrayOfInt[i + 1];
