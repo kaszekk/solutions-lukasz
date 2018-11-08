267b0efe65c95174cec499f85d32c32b0f886229
Lukasz <lukmyrta@gmail.com>
2018-11-08 15:46:27 +0100

    Tested version,includes printing the result.

---
+++ src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
@@ -0,0 +1,34 @@
+package pl.coderstrust.sieve;
+
+import java.util.Arrays;
+
+public class SieveOfEratosthenes {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(sieve(100)));
+    }
+
+    public static int[] sieve(int maximumNumber) {
+        int[] array = new int[maximumNumber + 1];
+        for (int i = 2; i <= maximumNumber; i++) {
+            array[i] = i;
+        }
+        for (int multiplicator = 2; multiplicator <= maximumNumber; multiplicator++) {
+            for (int sieveNumber = 2; sieveNumber * multiplicator <= maximumNumber; sieveNumber++) {
+                array[sieveNumber * multiplicator] = 0;
+            }
+        }
+        int primaryNumbersCounter = 0;
+        for (int element : array) {
+            if (element != 0) {
+                primaryNumbersCounter++;
+            }
+        }
+        int[] primaryNumbersArray = new int[primaryNumbersCounter];
+        for (int i = 0, j = 0; i < array.length; i++) {
+            if (array[i] != 0) {
+                primaryNumbersArray[j++] = array[i];
+            }
+        }
+        return primaryNumbersArray;
+    }
+}
