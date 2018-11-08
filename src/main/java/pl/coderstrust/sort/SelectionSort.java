264b8f3bcd55e0eaf563a50e66a0a446afaf4042
Lukasz <lukmyrta@gmail.com>
2018-11-08 01:32:11 +0100

    Selection sort working.may need optimalization, returns an array

--- src/main/java/pl/coderstrust/sort/SelectionSort.java
+++ src/main/java/pl/coderstrust/sort/SelectionSort.java
@@ -1,30 +1,38 @@
 package pl.coderstrust.sort;
 
+import java.util.Arrays;
+
 public class SelectionSort {
     public static void main(String[] args) {
         int[] array = {-7, 6, 7, 2, 3, 1, 0, 5, 12, -4, 9, 8};
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
+        for (int element : array) {
+            System.out.print(element + " ");
         }
         System.out.println();
-        sort(array);
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
+        System.out.println(Arrays.toString(sort(array)));
+        for (int element : array) {
+            System.out.print(element + " ");
         }
     }
 
     public static int[] sort(int[] array) {
+
+        int[] copyOfArray = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            copyOfArray[i] = array[i];
+        }
+
         for (int j = 0; j < array.length; j++) {
             int minIndex = j;
             for (int i = j; i < array.length - 1; i++) {
-                if (array[i + 1] < array[minIndex]) {
+                if (copyOfArray[i + 1] < copyOfArray[minIndex]) {
                     minIndex = i + 1;
                 }
             }
-            int temp = array[minIndex];
-            array[minIndex] = array[j];
-            array[j] = temp;
+            int temp = copyOfArray[minIndex];
+            copyOfArray[minIndex] = copyOfArray[j];
+            copyOfArray[j] = temp;
         }
-        return array;
+        return copyOfArray;
     }
 }
