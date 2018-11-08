c56ada0b131a2bb21deb84c2e289148b832cbc64
Lukasz <lukmyrta@gmail.com>
2018-11-08 17:24:53 +0100

    without extracting methods, ready to push

--- src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
+++ src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
@@ -6,35 +6,23 @@ public class SieveOfEratosthenes {
         for (int i = 2; i <= maximumNumber; i++) {
             array[i] = i;
         }
-        return removeZerosFromArray(applySieve(maximumNumber, array));
-    }
-
-    private static int[] removeZerosFromArray(int[] array) {
-        int[] primaryNumbersArray = new int[getPrimaryNumbersNumber(array)];
-        for (int i = 0, j = 0; i < array.length; i++) {
-            if (array[i] != 0) {
-                primaryNumbersArray[j++] = array[i];
-            }
-        }
-        return primaryNumbersArray;
-    }
-
-    private static int[] applySieve(int maximumNumber, int[] array) {
         for (int multiplicator = 2; multiplicator <= maximumNumber; multiplicator++) {
             for (int sieveNumber = 2; sieveNumber * multiplicator <= maximumNumber; sieveNumber++) {
                 array[sieveNumber * multiplicator] = 0;
             }
         }
-        return array;
-    }
-
-    private static int getPrimaryNumbersNumber(int[] array) {
         int primaryNumbersCounter = 0;
         for (int element : array) {
             if (element != 0) {
                 primaryNumbersCounter++;
             }
         }
-        return primaryNumbersCounter;
+        int[] primaryNumbersArray = new int[primaryNumbersCounter];
+        for (int i = 0, j = 0; i < array.length; i++) {
+            if (array[i] != 0) {
+                primaryNumbersArray[j++] = array[i];
+            }
+        }
+        return primaryNumbersArray;
     }
 }
