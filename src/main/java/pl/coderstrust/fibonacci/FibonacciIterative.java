996fc595987875e6577dece4999c0721e476ade6
Lukasz <lukmyrta@gmail.com>
2018-11-10 00:45:23 +0100

    needs exception

--- src/main/java/pl/coderstrust/fibonacci/FibonacciIterative.java
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciIterative.java
@@ -1,14 +1,14 @@
 package pl.coderstrust.fibonacci;
 
 public class FibonacciIterative {
-    public static long fibonacci(int fibonacciNumberInOrder) {
+    private static long fibonacci(int fibonacciNumberInOrder) {
         long[] fib = {1, 1};
-        long fibValue = 1;
+        long result = 1;
         for (int i = 3; i <= fibonacciNumberInOrder; i++) {
-            fibValue = fib[1] + fib[0];
+            result = fib[1] + fib[0];
             fib[0] = fib[1];
-            fib[1] = fibValue;
+            fib[1] = result;
         }
-        return fibValue;
+        return result;
     }
 }
