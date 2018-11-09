e12f19ad1eb02a95b68c1deedc9a874622640132
Łukasz Rosół <lrosol@users.noreply.github.com>
2018-11-09 22:08:40 +0100

    lon fibValue renamed to  long result
    Co-Authored-By: kaszekk <43524090+kaszekk@users.noreply.github.com>

--- src/main/java/pl/coderstrust/fibonacci/FibonacciIterative.java
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciIterative.java
@@ -3,7 +3,7 @@ package pl.coderstrust.fibonacci;
 public class FibonacciIterative {
     public static long fibonacci(int fibonacciNumberInOrder) {
         long[] fib = {1, 1};
-        long fibValue = 1;
+        long result = 1;
         for (int i = 3; i <= fibonacciNumberInOrder; i++) {
             fibValue = fib[1] + fib[0];
             fib[0] = fib[1];
