719c8e17f5893eda9ee7146014ee965ecf4f8013
Lukasz <lukmyrta@gmail.com>
2018-11-09 21:59:03 +0100

    Refactored.

--- src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
+++ src/main/java/pl/coderstrust/sieve/SieveOfEratosthenes.java
@@ -1,28 +1,46 @@
 package pl.coderstrust.sieve;
 
 public class SieveOfEratosthenes {
-    public static int[] sieve(int maximumNumber) {
-        int[] array = new int[maximumNumber + 1];
-        for (int i = 2; i <= maximumNumber; i++) {
-            array[i] = i;
-        }
+    static int[] sieve(int maximumNumber) {
+        int[] array = generateSequenceOfNumbers(maximumNumber);
+        filterPrimes(maximumNumber, array);
+        int[] primaryNumbersArray = new int[getPrimaryNumbersCount(array)];
+
+        return getPrimesArray(array, primaryNumbersArray);
+    }
+
+    private static void filterPrimes(int maximumNumber, int[] array) {
         for (int multiplicator = 2; multiplicator <= maximumNumber; multiplicator++) {
             for (int sieveNumber = 2; sieveNumber * multiplicator <= maximumNumber; sieveNumber++) {
                 array[sieveNumber * multiplicator] = 0;
             }
         }
-        int primaryNumbersCounter = 0;
-        for (int element : array) {
-            if (element != 0) {
-                primaryNumbersCounter++;
-            }
-        }
-        int[] primaryNumbersArray = new int[primaryNumbersCounter];
+    }
+
+    private static int[] getPrimesArray(int[] array, int[] primaryNumbersArray) {
         for (int i = 0, j = 0; i < array.length; i++) {
             if (array[i] != 0) {
                 primaryNumbersArray[j++] = array[i];
             }
         }
         return primaryNumbersArray;
+    }
+
+    private static int getPrimaryNumbersCount(int[] array) {
+        int primaryNumbersCounter = 0;
+        for (int element : array) {
+            if (element != 0) {
+                primaryNumbersCounter++;
+            }
+        }
+        return primaryNumbersCounter;
+    }
+
+    private static int[] generateSequenceOfNumbers(int maximumNumber) {
+        int[] array = new int[maximumNumber + 1];
+        for (int i = 2; i <= maximumNumber; i++) {
+            array[i] = i;
+        }
+        return array;
     }
 }
