0a84d62100a072240c7426251710c8af8910b891
Lukasz <lukmyrta@gmail.com>
2018-11-14 13:11:05 +0100

    exception,result including formatting tested

--- src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
+++ src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
@@ -10,23 +10,21 @@ public class MultiplicationTable {
 
     static List<String> getResult(int size) throws IllegalArgumentException {
         if (size < 0) {
-            throw new IllegalArgumentException("Multiplication table size cannot be negative");
+            throw new IllegalArgumentException("Size argument cannot be negative");
         }
         List<String> result = new ArrayList<>();
         StringBuilder item = new StringBuilder();
-        // item.append(String.format("%4s", " "));
+        item.append(String.format("%4s", " "));
         for (int i = 1; i <= size; i++) {
-            item.append(i);
+            item.append(String.format("%4d", i));
         }
-        // item.append("\n");
         result.add(item.toString());
         item.delete(0, item.length());
         for (int row = 1; row <= size; row++) {
-            item.append(row);
+            item.append(String.format("%4d", row));
             for (int col = 1; col <= size; col++) {
-                item.append(row * col);
+                item.append(String.format("%4d", row * col));
             }
-            // item.append("\n");
             result.add(item.toString());
             item.delete(0, item.length());
         }
--- src/test/java/pl/coderstrust/multiplication/MultiplicationTableTest.java
+++ src/test/java/pl/coderstrust/multiplication/MultiplicationTableTest.java
@@ -1,57 +1,46 @@
 package pl.coderstrust.multiplication;
 
+import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import static junit.framework.TestCase.assertEquals;
 import static org.hamcrest.CoreMatchers.is;
 
-//import org.junit.Test;
-
-@RunWith(Parameterized.class)
+@RunWith(JUnitParamsRunner.class)
 public class MultiplicationTableTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Test
     @Parameters(method = "getExpectedParameters")
-    public static void shouldCalculateMultiplicationTable(int size, Object expected) {
+    public static void shouldReturnCorrectMultiplicationTableList(int size, Object expected) {
         // When
         List<String> input = MultiplicationTable.getResult(size);
         // Then
-        assertEquals(expected, MultiplicationTable.getResult(size));
         Assert.assertThat(input, is(expected));
     }
 
     public static Object[] getExpectedParameters() {
         return new Object[]{
-                new Object[]{0, ""},
-                new Object[]{1, "1", "11"},
-                new Object[]{2, "12", "112", "224"},
-                new Object[]{4, "1234", "11234", "22468", "336912", "4481216"}};
+                new Object[]{0, new ArrayList<>(Arrays.asList("    "))},
+                new Object[]{1, new ArrayList<>(Arrays.asList("       1", "   1   1"))},
+                new Object[]{2, new ArrayList<>(Arrays.asList("       1   2", "   1   1   2", "   2   2   4"))},
+                new Object[]{4, new ArrayList<>(Arrays.asList("       1   2   3   4", "   1   1   2   3   4", "   2   2   4   6   8", "   3   3   6   9  12", "   4   4   8  12  16"))}
+        };
+    }
+
+    @Test
+    public void shouldThrowExceptionForInvalidArgument() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Size argument cannot be negative");
+        MultiplicationTable.getResult(-1);
     }
 }
-
-
-   /* List<String> strings =
-            new ArrayList<String>(Arrays.asList(new String[]{"one", "two", "three"}));*/
-
-  /*  @Test
-    @Parameters(method = "named1")
-    public void paramsInNamedMethod(String p1, Integer p2) { }
-    private Object named1() {
-        return new Object[]{"AAA", 1};
-    }*/
-
-   /* @Test
-    public void whenNegativeSizeShouldCatchIllegalArgumentException() {
-        //Given
-        int size = -1;
-        try {
-            List<String> input = MultiplicationTable.getResult(size);
-            fail("Expected an InvalidArgumentException to be thrown");
-        } catch (IllegalArgumentException ex) {
-            Assert.assertThat(ex.getMessage(), is());
-        }*/
