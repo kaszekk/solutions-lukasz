f737a6c9ba8e1b64eb4041a81b5528b0b6696ef6
Lukasz <lukmyrta@gmail.com>
2018-11-21 13:15:06 +0100

    F(0)=1,  F(1)=1,  F(2)=2  version implemented. Now throws exception for negative arguments.

--- src/main/java/pl/coderstrust/fibonacci/FibonacciIterative.java
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciIterative.java
@@ -1,10 +1,13 @@
 package pl.coderstrust.fibonacci;
 
 public class FibonacciIterative {
-    private static long fibonacci(int fibonacciNumberInOrder) {
+    static long fibonacci(int fibonacciNumberInOrder) {
+        if (fibonacciNumberInOrder < 0) {
+            throw new IllegalArgumentException("Cannot accept negative argument");
+        }
         long[] fib = {1, 1};
         long result = 1;
-        for (int i = 3; i <= fibonacciNumberInOrder; i++) {
+        for (int i = 2; i <= fibonacciNumberInOrder; i++) {
             result = fib[1] + fib[0];
             fib[0] = fib[1];
             fib[1] = result;
--- src/main/java/pl/coderstrust/fibonacci/FibonacciRecursive.java
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciRecursive.java
@@ -1,8 +1,11 @@
 package pl.coderstrust.fibonacci;
 
 public class FibonacciRecursive {
-    public static long fibonacci(int fibonacciNumberInOrder) {
-        if (fibonacciNumberInOrder == 1 || fibonacciNumberInOrder == 2) {
+    static long fibonacci(int fibonacciNumberInOrder) {
+        if (fibonacciNumberInOrder < 0) {
+            throw new IllegalArgumentException("Cannot accept negative argument");
+        }
+        if (fibonacciNumberInOrder == 0 || fibonacciNumberInOrder == 1) {
             return 1L;
         } else {
             return fibonacci(fibonacciNumberInOrder - 1) + fibonacci(fibonacciNumberInOrder - 2);
