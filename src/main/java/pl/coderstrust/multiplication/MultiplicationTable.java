df0e3b7fd81a85cdf3e1913d367b92add105afa3
Lukasz <lukmyrta@gmail.com>
2018-11-25 12:28:20 +0100

    before pull from remote branch

--- src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
+++ src/main/java/pl/coderstrust/multiplication/MultiplicationTable.java
@@ -2,54 +2,21 @@ package pl.coderstrust.multiplication;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
-        int size = 4;
-        printMultiplicationTable(getFormattedOutput(getMultiplicationTableValues(size)));
+        printMultiplicationTable(12);
     }
 
-    static int[][] getMultiplicationTableValues(int size) {
-        if (size < 0) {
-            throw new IllegalArgumentException("Argument cannot be negative");
+    private static void printMultiplicationTable(int size) {
+        System.out.printf("%4s", " ");
+        for (int i = 1; i <= size; i++) {
+            System.out.printf("%4d", i);
         }
-        int[][] multiplicationTable = new int[size + 1][size + 1];
+        System.out.println();
         for (int row = 1; row <= size; row++) {
+            System.out.printf("%4d", row);
             for (int col = 1; col <= size; col++) {
-                multiplicationTable[row][col] = row * col;
+                System.out.printf("%4d", row * col);
             }
-        }
-        return multiplicationTable;
-    }
-
-    static String[] getFormattedOutput(int[][] table) {
-        String[] formattedTableRow = new String[table.length];
-        StringBuilder tableRowBuilder = new StringBuilder();
-        addTopAndLeftBorders(table);
-        tableRowBuilder.append((String.format("%4s", " ")));
-        for (int col = 1; col < formattedTableRow.length; col++) {
-            tableRowBuilder.append(String.format("%4s", +table[0][col]));
-        }
-        formattedTableRow[0] = tableRowBuilder.toString();
-        for (int row = 1; row < table.length; row++) {
-            {
-                tableRowBuilder.delete(0, tableRowBuilder.length());
-                for (int col = 0; col < table.length; col++) {
-                    tableRowBuilder.append(String.format("%4s", +table[row][col]));
-                }
-                formattedTableRow[row] = tableRowBuilder.toString();
-            }
-        }
-        return formattedTableRow;
-    }
-
-    static void printMultiplicationTable(String[] table) {
-        for (String element : table) {
-            System.out.println(element);
-        }
-    }
-
-    private static void addTopAndLeftBorders(int[][] table) {
-        for (int col = 0; col < table.length; col++) {
-            table[0][col] = col;
-            table[col][0] = col;
+            System.out.println();
         }
     }
 }
