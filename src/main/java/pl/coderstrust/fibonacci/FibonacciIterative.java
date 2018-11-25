f9452549ef8ed9bba6142fbbce0be71c1d29771a
Lukasz <lukmyrta@gmail.com>
2018-11-25 20:09:52 +0100

    Additional changes to be applied

--- src/main/java/pl/coderstrust/fibonacci/FibonacciIterative.java
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciIterative.java
@@ -3,7 +3,7 @@ package pl.coderstrust.fibonacci;
 public class FibonacciIterative {
     static long fibonacci(int fibonacciNumberInOrder) {
         if (fibonacciNumberInOrder < 0) {
-            throw new IllegalArgumentException("Cannot accept negative argument");
+            throw new IllegalArgumentException("fibonacciNumberInOrder argument must be greater than zero");
         }
         long[] fib = {1, 1, 1};
         for (int i = 2; i <= fibonacciNumberInOrder; i++) {
--- src/main/java/pl/coderstrust/fibonacci/FibonacciRecursive.java
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciRecursive.java
@@ -3,9 +3,9 @@ package pl.coderstrust.fibonacci;
 public class FibonacciRecursive {
     static long fibonacci(int fibonacciNumberInOrder) {
         if (fibonacciNumberInOrder < 0) {
-            throw new IllegalArgumentException("Cannot accept negative argument");
+            throw new IllegalArgumentException("fibonacciNumberInOrder argument must be greater than zero");
         }
-        if (fibonacciNumberInOrder == 0 || fibonacciNumberInOrder == 1) {
+        if (fibonacciNumberInOrder <= 2) {
             return 1L;
         } else {
             return fibonacci(fibonacciNumberInOrder - 1) + fibonacci(fibonacciNumberInOrder - 2);
