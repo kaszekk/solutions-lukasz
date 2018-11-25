999755552d8b74916a993ab78c05e1cf51d9322e
Lukasz <lukmyrta@gmail.com>
2018-11-25 18:33:55 +0100

    Corrections suggested by Mentor applied.

--- src/test/java/pl/coderstrust/isThatFibonacci/FibonacciCheckerTest.java
+++ src/test/java/pl/coderstrust/isThatFibonacci/FibonacciCheckerTest.java
@@ -7,35 +7,57 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class FibonacciCheckerTest {
 
     @ParameterizedTest
-    @MethodSource("paramsForFibonacciChecker")
-    void shouldReturnTrueIfGivenNumberIsFibonacci(long n, boolean isFibonacci) {
+    @MethodSource("paramsForFibonacciNumber")
+    void shouldReturnTrueForFibonacciNumber(long n, boolean isFibonacci) {
         FibonacciChecker fibCheck = new FibonacciChecker();
-        //Given
-        boolean expected = isFibonacci;
 
         //When
-        boolean input = fibCheck.isFibonacciNumber(n);
+        boolean actual = fibCheck.isFibonacciNumber(n);
 
         //Then
-        assertThat(input, is(expected));
+        assertTrue(actual);
     }
 
-    private static Stream<Arguments> paramsForFibonacciChecker() {
+    private static Stream<Arguments> paramsForFibonacciNumber() {
         return Stream.of(
+                Arguments.of(0, true),
                 Arguments.of(1, true),
                 Arguments.of(2, true),
-                Arguments.of(4, false),
+                Arguments.of(3, true),
                 Arguments.of(5, true),
-                Arguments.of(10, false),
                 Arguments.of(13, true),
-                Arguments.of(15, false)
+                Arguments.of(21, true),
+                Arguments.of(34, true),
+                Arguments.of(10946, true)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("paramsForNonFibonacciNumber")
+    void shouldReturnFalseForNonFibonacciNumber(long n, boolean isFibonacci) {
+        FibonacciChecker fibCheck = new FibonacciChecker();
+
+        //When
+        boolean actual = fibCheck.isFibonacciNumber(n);
+
+        //Then
+        assertFalse(actual);
+    }
+
+    private static Stream<Arguments> paramsForNonFibonacciNumber() {
+        return Stream.of(
+                Arguments.of(4, false),
+                Arguments.of(6, false),
+                Arguments.of(10, false),
+                Arguments.of(15, false),
+                Arguments.of(4180, false),
+                Arguments.of(9180, false),
+                Arguments.of(10947, false)
         );
     }
 
