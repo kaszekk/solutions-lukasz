817b28ba75e4f5712f22422186270ac1a46a16c5
Lukasz <lukmyrta@gmail.com>
2018-11-25 11:10:11 +0100

    got rid result variable, instead fib[2] used to store result

--- src/main/java/pl/coderstrust/fibonacci/FibonacciIterative.java
+++ src/main/java/pl/coderstrust/fibonacci/FibonacciIterative.java
@@ -5,13 +5,12 @@ public class FibonacciIterative {
         if (fibonacciNumberInOrder < 0) {
             throw new IllegalArgumentException("Cannot accept negative argument");
         }
-        long[] fib = {1, 1};
-        long result = 1;
+        long[] fib = {1, 1, 1};
         for (int i = 2; i <= fibonacciNumberInOrder; i++) {
-            result = fib[1] + fib[0];
+            fib[2] = fib[1] + fib[0];
             fib[0] = fib[1];
-            fib[1] = result;
+            fib[1] = fib[2];
         }
-        return result;
+        return fib[2];
     }
 }
