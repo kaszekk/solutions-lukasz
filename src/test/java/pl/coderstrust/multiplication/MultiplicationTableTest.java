66acf05026858fdc9ea2dbfd36321b2c99c38b7e
Lukasz <lukmyrta@gmail.com>
2018-11-30 15:34:05 +0100

    needs dimension test

--- src/test/java/pl/coderstrust/multiplication/MultiplicationTableTest.java
+++ src/test/java/pl/coderstrust/multiplication/MultiplicationTableTest.java
@@ -19,34 +19,39 @@ import static pl.coderstrust.multiplication.MultiplicationTable.getMultiplicatio
 class MultiplicationTest {
     @DisplayName("Should return multiplication result as int [][] of correct dimensions ")
     @ParameterizedTest(name = "{index} => a={0},   b={1},   expected_a={2},     expected_b={2} ")
-    @MethodSource("paramsForAreDimensionsCorrect")
-    void shouldReturnMultiplicationTableResultAsArrayOfArrayOfCorrectDimensions(int a, int b, int expected_a, int expected_b) {
+    @MethodSource("paramsForTestMultiplicationTableDimensions")
+    void testMultiplicationTableDimensions(int a, int b, int expected_a, int expected_b) {
 
         //Given
-        int size = a >= b ? a : b;
+        int size = 4;
         int[][] returned = getMultiplicationTableValues(size);
         int resultDimension_a = returned.length - 1;
         int resultDimension_b = returned[b].length - 1;
 
         //When
-        boolean areDimensionsCorrect = (resultDimension_a == expected_a) && (resultDimension_b == expected_b);
+        boolean is_a_sizeCorrect = resultDimension_a == expected_a;
+        boolean is_b_sizeCorrect = resultDimension_b == expected_b;
+        boolean isTableSquare = resultDimension_a == resultDimension_b;
 
         //Then
-        assertTrue(areDimensionsCorrect);
+        assertTrue(is_a_sizeCorrect);
+        assertTrue(is_b_sizeCorrect);
+        assertTrue(isTableSquare);
+
+
     }
 
-    private static Stream<Arguments> paramsForAreDimensionsCorrect() {
+    private static Stream<Arguments> paramsForTestMultiplicationTableDimensions() {
         return Stream.of(
-                Arguments.of(0, 0, 0, 0),
-                Arguments.of(4, 0, 4, 4),
-                Arguments.of(1, 1, 1, 1),
-                Arguments.of(2, 1, 2, 2),
-                Arguments.of(3, 2, 3, 3),
+                Arguments.of(0, 4, 4, 4),
+                Arguments.of(1, 4, 4, 4),
+                Arguments.of(2, 4, 4, 4),
                 Arguments.of(3, 4, 4, 4),
-                Arguments.of(4, 4, 4, 4),
+                Arguments.of(4, 0, 4, 4),
+                Arguments.of(4, 1, 4, 4),
+                Arguments.of(4, 2, 4, 4),
                 Arguments.of(4, 3, 4, 4),
-                Arguments.of(2, 2, 2, 2),
-                Arguments.of(3, 3, 3, 3)
+                Arguments.of(4, 4, 4, 4)
         );
     }
 
@@ -56,7 +61,7 @@ class MultiplicationTest {
     void shouldReturnCorrectMultiplicationResult(int a, int b, int expected) {
 
         //Given
-        int size = a >= b ? a : b;
+        int size = 12;
 
         //When
         int[][] result = getMultiplicationTableValues(size);
@@ -83,7 +88,7 @@ class MultiplicationTest {
     @DisplayName("Should return correctly formatted MultiplicationTable for given size")
     @ParameterizedTest(name = "{index} => size={0},   expected={1}")
     @MethodSource("paramsForMultiplicationTableFormattedOutput")
-    void shouldReturnCorrectlyFormattedMultiplicationTableForGivenSize(int size, String[] expected) {
+    void testFormatOfMultiplicationTable(int size, String[] expected) {
 
         //When
         String[] result = getFormattedOutput(getMultiplicationTableValues(size));
@@ -111,7 +116,7 @@ class MultiplicationTest {
     }
 
     @Test
-    void shouldThrowIllegalArgumentExceptionForNegativeSize() {
+    void throwExceptionForNegativeSize() {
         assertThrows(IllegalArgumentException.class,
                 () -> {
                     int size = -1;
