e9e15a912f3c2e4398432ca1b83bc84dfe0a4dc3
Lukasz <lukmyrta@gmail.com>
2018-12-02 21:51:30 +0100

    all OK, now need get Junit5 test for FoobarTest

--- src/main/java/pl/coderstrust/pascal/PascalTriangle.java
+++ src/main/java/pl/coderstrust/pascal/PascalTriangle.java
@@ -1,56 +1,27 @@
 package pl.coderstrust.pascal;
 
-public class PascalTriangle {
-    public static void main(String[] args) {
-        printPascalTriangle(0);
-    }
+import java.util.ArrayList;
+import java.util.List;
 
-    static int[][] getCalculatedValues(int numberOfRows) {
+public class PascalTriangle {
+
+    public static List<String> getPascalTriangle(int numberOfRows) {
         if (numberOfRows <= 0) {
             throw new IllegalArgumentException("Number of rows must be greater than zero");
         }
-        int[][] triangleValues = new int[numberOfRows][];
-        createRightSizedArraysForSubsequentRowsOfTriangle(numberOfRows, triangleValues);
-        calculateAndSaveValues(numberOfRows, triangleValues);
-        return triangleValues;
-    }
-
-    static String[] getFormattedOutput(int[][] values) {    ////
-        String[] formattedOutput = new String[values.length];
-        
-        StringBuilder sb = new StringBuilder();
-        for (int row = 0; row < values.length; row++) {
-            final int rowIndentation = (values.length - row) * 2;
-            sb.append(String.format("%" + rowIndentation + "s", ""));
-            for (int col = 0; col < values[row].length; col++) {
-                sb.append(String.format("%4d", values[row][col]));
-            }
-            formattedOutput[row] = sb.toString();
-            sb.delete(0, sb.length());
-        }
-        return formattedOutput;
-    }
-
-    private static void printPascalTriangle(int numberOfRows) {
-        String[] formattedOutput = getFormattedOutput(getCalculatedValues(numberOfRows));
-        for (String element : formattedOutput) {
-            System.out.println(element);
-        }
-    }
-
-    private static void createRightSizedArraysForSubsequentRowsOfTriangle(int numberOfRows, int[][] triangleValues) {   //////
-        for (int i = 0; i < numberOfRows; i++) {
-            triangleValues[i] = new int[i + 1];
-        }
-    }
-
-    private static void calculateAndSaveValues(int numberOfRows, int[][] triangleValues) {    ////////
+        List<String> pascalTriangleValues = new ArrayList<>();
+        StringBuilder pascalTriangleRowOfValuesBuilder = new StringBuilder();
         for (int row = 0; row < numberOfRows; row++) {
+            pascalTriangleRowOfValuesBuilder.delete(0, pascalTriangleRowOfValuesBuilder.length());
             int value = 1;
-            for (int col = 0; col <= triangleValues[row].length - 1; col++) {
-                triangleValues[row][col] = value;
+            pascalTriangleRowOfValuesBuilder.append(value);
+            for (int col = 0; col < row; col++) {
                 value = value * (row - col) / (col + 1);
+                pascalTriangleRowOfValuesBuilder.append(" ");
+                pascalTriangleRowOfValuesBuilder.append(value);
             }
+            pascalTriangleValues.add(pascalTriangleRowOfValuesBuilder.toString());
         }
+        return pascalTriangleValues;
     }
 }
--- src/test/java/pl/coderstrust/pascal/PascalTriangleTest.java
+++ src/test/java/pl/coderstrust/pascal/PascalTriangleTest.java
@@ -1,88 +1,67 @@
 package pl.coderstrust.pascal;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static pl.coderstrust.pascal.PascalTriangle.getCalculatedValues;
-import static pl.coderstrust.pascal.PascalTriangle.getFormattedOutput;
+import static pl.coderstrust.pascal.PascalTriangle.getPascalTriangle;
+
 
 @DisplayName("PascalTriangle test ")
 class PascalTriangleTest {
 
-    @DisplayName("Should calculate the correct values for given [row] [column] of Pascal Triangle")
-    @ParameterizedTest(name = "{index} => row={0}, col={1}, expected={2}")
-    @MethodSource("parametersForPascalTriangleValues")
-    void shouldReturnCorrectPascalTriangleValueForGivenRowAndColumn(int row, int col, int expected) {
-
-        //Given
-        int numberOfRows = row + 1;
-
-        //When
-        int[][] input = getCalculatedValues(numberOfRows);
-
-        //Then
-        assertThat(input[row][col], is(expected));
-    }
-
-    private static Stream<Arguments> parametersForPascalTriangleValues() {
-        return Stream.of(
-                Arguments.of(0, 0, 1),
-                Arguments.of(4, 0, 1),
-                Arguments.of(1, 1, 1),
-                Arguments.of(2, 1, 2),
-                Arguments.of(3, 2, 3),
-                Arguments.of(11, 5, 462),
-                Arguments.of(4, 4, 1),
-                Arguments.of(6, 6, 1),
-                Arguments.of(9, 4, 126)
-        );
-    }
-
-    @DisplayName("Should return correctly formatted PascalTriangle for given numberOfRows")
+    @DisplayName("Should return correct PascalTriangle values in List<String> for given numberOfRows")
     @ParameterizedTest(name = "{index} => numberOfRows={0}, expected={1}")
-    @MethodSource("parametersForPascalTriangleFormattedOutput")
-    void shouldReturnCorrectlyPascalTriangleForGivenSize(int numberOfRows, String[] expected) {
+    @MethodSource("parametersForValuesTest")
+    void valuesTest(int numberOfRows, List<String> expected) {
 
         //When
 
-        String[] input = getFormattedOutput(getCalculatedValues(numberOfRows));
+        List<String> actual = getPascalTriangle(numberOfRows);
 
         //Then
 
-        assertThat(input, is(expected));
+        assertEquals(expected, actual);
     }
 
-    private static Stream<Object> parametersForPascalTriangleFormattedOutput() {
+    private static Stream<Object> parametersForValuesTest() {
         return Stream.of(
                 Arguments.of(1,
-                        new String[]{"     1"}),
+                        new ArrayList<>(Arrays.asList
+                                ("1"))),
                 Arguments.of(2,
-                        new String[]{
-                                "       1",
-                                "     1   1"}),
+                        new ArrayList<>(Arrays.asList
+                                (("1"),
+                                 ("1 1")))),
                 Arguments.of(4,
-                        new String[]{
-                                "           1",
-                                "         1   1",
-                                "       1   2   1",
-                                "     1   3   3   1",
-                        }));
+                        new ArrayList<>(Arrays.asList
+                                (("1"),
+                                 ("1 1"),
+                                 ("1 2 1"),
+                                 ("1 3 3 1")))));
     }
 
-    @Test
-    void shouldThrowIllegalArgumentExceptionForNegativeSize() {
+    @DisplayName("Should throw Exception for zero or negative numberOfRows")
+    @ParameterizedTest(name = "{index} => numberOfRows={0}")
+    @MethodSource("parametersForExceptionTest")
+    void exceptionTest(int numberOfRows) {
         assertThrows(IllegalArgumentException.class,
                 () -> {
-                    int numberOfRows = -1;
-                    getCalculatedValues(numberOfRows);
+                    getPascalTriangle(numberOfRows);
                 });
+    }
+
+    private static Stream<Object> parametersForExceptionTest() {
+        return Stream.of(
+                Arguments.of(0),
+                Arguments.of(-1));
     }
 }
