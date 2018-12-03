dd74938f34e0fac44d3a47d176bb1fb8d26b2855
Lukasz <lukmyrta@gmail.com>
2018-12-03 09:17:35 +0100

    ChristmasTree testable added in order to migrate to Junit5 - test structure added

---
+++ src/test/java/pl/coderstrust/christmas/ChristmasTreeTest.java
@@ -0,0 +1,12 @@
+package pl.coderstrust.christmas;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChristmasTreeTest {
+
+    @Test
+    void getChristmasTree() {
+    }
+}
