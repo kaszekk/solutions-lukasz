05584008c2e5323a1de566c60e7b6facdc171ab2
Lukasz <lukmyrta@gmail.com>
2018-12-05 23:13:37 +0100

    basic structure added

--- src/main/java/pl/coderstrust/Regex/Regex.java
+++ src/main/java/pl/coderstrust/Regex/Regex.java
@@ -1,9 +1,9 @@
 package pl.coderstrust.Regex;
 
 public class Regex {
-    boolean isIpV4AddressValid(String ipV4Address){
+    boolean isIpV4AddressValid(String ipV4Address) {
         String ipV4RegexPattern = "";
-    matcher.compile(ipV4RegexPattern);
+        matcher.compile(ipV4RegexPattern);
     }
 
 }
