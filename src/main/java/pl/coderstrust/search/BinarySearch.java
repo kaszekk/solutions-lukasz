0de9ded5f2e86df86f79a106c52b66f2d53011ae
Lukasz <lukmyrta@gmail.com>
2018-12-06 19:54:43 +0100

    binary search OK

--- src/main/java/pl/coderstrust/search/BinarySearch.java
+++ src/main/java/pl/coderstrust/search/BinarySearch.java
@@ -5,42 +5,23 @@ public class BinarySearch {
     public static void main(String[] args) {
         int[] sortedArray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22};
         BinarySearch binarySearch = new BinarySearch();
-        System.out.println(binarySearch.search(sortedArray, 18));
+        System.out.println(binarySearch.search(sortedArray, 4));
     }
 
-    int search(int[] sortedArray, int lookedFor) {
-        int numberOfPicks = 0;
-
-        int prevDivIndex = sortedArray.length - 1;
-        int divIndex = prevDivIndex / 2;
-        numberOfPicks++;
-
-        while (divIndex != prevDivIndex) {
-            if (sortedArray[divIndex] > lookedFor) {
-                prevDivIndex = divIndex;
-                divIndex /= 2;
-                numberOfPicks++;
-            } else if (sortedArray[divIndex] < lookedFor) {
-                prevDivIndex = divIndex;
-                divIndex = divIndex + (sortedArray.length - divIndex) / 2;
-                numberOfPicks++;
-            } else if (sortedArray[divIndex] == lookedFor) {
-                System.out.println("Found in " + numberOfPicks + " picks");
-                return divIndex;
+    int search(int[] sortedArray, int searched) {
+        int low = 0;
+        int high = sortedArray.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (sortedArray[mid] == searched) {
+                return mid;
+            }
+            if (sortedArray[mid] > searched) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
         return -1;
     }
 }
-
-
-/*
-
-    int search(int[] sortedArray, int lookedFor) {
-        for (int i = 0; i < sortedArray.length; i++) {
-            if (sortedArray[i] == lookedFor) {
-                return i;
-            }
-        }
-        return -1;
-    }*/
