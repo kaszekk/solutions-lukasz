5644c7b497dd694b4229423d526b69e6d0995ab0
Lukasz <lukmyrta@gmail.com>
2018-12-06 15:37:20 +0100

    debugging

--- src/main/java/pl/coderstrust/search/BinarySearch.java
+++ src/main/java/pl/coderstrust/search/BinarySearch.java
@@ -3,40 +3,35 @@ package pl.coderstrust.search;
 public class BinarySearch {
 
     public static void main(String[] args) {
-        int[] sortedArray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
+        int[] sortedArray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22};
         BinarySearch binarySearch = new BinarySearch();
-        System.out.println(binarySearch.search(sortedArray, 2));
+        System.out.println(binarySearch.search(sortedArray, 18));
     }
 
     int search(int[] sortedArray, int lookedFor) {
         int numberOfPicks = 0;
 
-        int prevDivIndex = sortedArray.length;
+        int prevDivIndex = sortedArray.length - 1;
         int divIndex = prevDivIndex / 2;
+        numberOfPicks++;
 
         while (divIndex != prevDivIndex) {
             if (sortedArray[divIndex] > lookedFor) {
                 prevDivIndex = divIndex;
-                divIndex = divIndex / 2;
+                divIndex /= 2;
                 numberOfPicks++;
-                if (sortedArray[divIndex] == lookedFor) {
-                    System.out.println("Found in " + numberOfPicks + " picks");
-                    return divIndex;
-                }
             } else if (sortedArray[divIndex] < lookedFor) {
-                // prevDivIndex = divIndex;
-                divIndex = (prevDivIndex + divIndex) / 2;
+                prevDivIndex = divIndex;
+                divIndex = divIndex + (sortedArray.length - divIndex) / 2;
                 numberOfPicks++;
-                if (sortedArray[divIndex] == lookedFor) {
-                    System.out.println("Found in " + numberOfPicks + " picks");
-                    return divIndex;
-                }
+            } else if (sortedArray[divIndex] == lookedFor) {
+                System.out.println("Found in " + numberOfPicks + " picks");
+                return divIndex;
             }
         }
         return -1;
     }
 }
-
 
 
 /*
