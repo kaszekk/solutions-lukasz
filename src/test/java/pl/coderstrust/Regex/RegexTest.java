f83e31b9c62bc803809ce8a4fc14f71e787cdc11
Lukasz <lukmyrta@gmail.com>
2018-12-07 08:48:52 +0100

    test Ok

--- src/test/java/pl/coderstrust/Regex/RegexTest.java
+++ src/test/java/pl/coderstrust/Regex/RegexTest.java
@@ -1,22 +1,38 @@
 package pl.coderstrust.Regex;
 
-import static java.lang.Math.pow;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RegexTest {
+    StringBuilder ip = new StringBuilder();
 
     @org.junit.jupiter.api.Test
     void isIpV4AddressTest() {
         //Given
-        final double maxForValue = pow(2, 32) - 1;
         Regex regex = new Regex();
-        String input = "011.255.255.255";
-        //When
-        boolean actual = regex.isIpV4Address(input);
-        assertTrue(actual);
-     /*   for (long ip = 0; ip <= maxForValue; ip++) {
-            //Then
-        }*/
+
+        for (int firstOctet = 0; firstOctet <= 255; firstOctet++) {
+            ip.append(String.format("%03d", firstOctet)).append(".");
+            for (int secondOctet = 0; secondOctet <= 255; secondOctet++) {
+                ip.append(String.format("%03d", secondOctet)).append(".");
+                for (int thirdOctet = 0; thirdOctet <= 255; thirdOctet++) {
+                    ip.append(String.format("%03d", thirdOctet)).append(".");
+                    for (int forthOctet = 0; forthOctet <= 255; forthOctet++) {
+                        if (forthOctet < 10) {
+                            ip.append(00).append(forthOctet);
+                        } else if (forthOctet < 100) {
+                            ip.append(0).append(forthOctet);
+                        } else {
+                            ip.append(forthOctet);
+                        }
+                        boolean actual = regex.isIpV4Address(ip.toString());
+                        assertTrue(actual);
+                        ip.delete(12, 15);
+                    }
+                    ip.delete(8, 15);
+                }
+                ip.delete(4, 15);
+            }
+            ip.delete(0, 15);
+        }
     }
 }
-
