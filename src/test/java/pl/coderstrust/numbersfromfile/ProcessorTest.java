02b877fd13b91e32a1000b08e12a1161e726072b
Lukasz <lukmyrta@gmail.com>
2018-12-09 20:53:17 +0100

    mocking works

--- src/test/java/pl/coderstrust/numbersfromfile/ProcessorTest.java
+++ src/test/java/pl/coderstrust/numbersfromfile/ProcessorTest.java
@@ -2,8 +2,6 @@ package pl.coderstrust.numbersfromfile;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -15,7 +13,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-@RunWith(JUnitPlatform.class)
+//@RunWith(JUnitPlatform.class)
 public class ProcessorTest {
 
     @Mock
@@ -37,9 +35,9 @@ public class ProcessorTest {
     void shouldProcessProvidedInputFileAndSaveResultToProvidedOutputFile() throws IOException {
 
         // Given
-        when(fileProcessor.readLinesFromFile("src/main/resources/Input.txt")).thenReturn(Arrays.asList("1 2 3", "4 5 6"));
+        when(fileProcessor.readLinesFromFile("src/main/resources/Input.txt")).thenReturn(Arrays.asList("1 2 3"));
         when(numbersProcessor.processLine("1 2 3")).thenReturn("1+2+3=6");
-        when(numbersProcessor.processLine("4 5 6")).thenReturn("4+5+6=15");
+        // when(numbersProcessor.processLine("4 5 6")).thenReturn("4+5+6=15");
 
         // When
         processor.process("src/main/resources/Input.txt", "src/main/resources/Output.txt");
