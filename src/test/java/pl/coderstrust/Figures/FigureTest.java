f32ce5df97f4fa9e4d9e76c7975552826418e3c5
Lukasz <lukmyrta@gmail.com>
2018-12-10 15:45:17 +0100

    all tests including polymorphism passing, now need use setters in tests

--- src/test/java/pl/coderstrust/Figures/FigureTest.java
+++ src/test/java/pl/coderstrust/Figures/FigureTest.java
@@ -1,10 +1,63 @@
 package pl.coderstrust.Figures;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class FigureTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+class FigureTest {
     @Test
-    void calculateArea() {
+    @DisplayName("Should show polymorphism")
+    void polymorphismTest() {
+
+        //Given
+        double actual;
+        Figure figure = new Circle(2);
+
+        //When
+        actual = figure.calculateArea();
+
+        //Then
+        assertEquals(new Circle(2).calculateArea(), actual);
+
+
+        //Given
+        figure = new Square(3);
+
+        //When
+        actual = figure.calculateArea();
+
+        //Then
+        assertEquals(new Square(3).calculateArea(), actual);
+
+
+        //Given
+        figure = new Rectangle(2, 4);
+
+        //When
+        actual = figure.calculateArea();
+
+        //Then
+        assertEquals(new Rectangle(2, 4).calculateArea(), actual);
+
+
+        //Given
+        figure = new Triangle(2, 3);
+
+        //When
+        actual = figure.calculateArea();
+
+        //Then
+        assertEquals(new Triangle(2, 3).calculateArea(), actual);
+
+
+        //Given
+        figure = new Trapezoid(1, 3, 4);
+
+        //When
+        actual = figure.calculateArea();
+
+        //Then
+        assertEquals(new Trapezoid(1, 3, 4).calculateArea(), actual);
     }
 }
