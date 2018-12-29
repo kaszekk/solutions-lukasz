fbd47dedb18f58ac9652ba7684904cef5cc88d6d
Lukasz <lukmyrta@gmail.com>
2018-12-29 20:10:17 +0100

    Suggested changes applied

--- src/main/java/pl/coderstrust/hoare/HoarePartition.java
+++ src/main/java/pl/coderstrust/hoare/HoarePartition.java
@@ -1,28 +1,29 @@
-package pl.coderstrust.hoarepartition;
+package pl.coderstrust.hoare;
 
 class HoarePartition {
-    static int[] applyHoare(int[] tableForPartition, int pivotIndex) {
+    static int[] partitionTable(int[] tableForPartition, int pivotIndex) {
         if (tableForPartition == null) {
-            throw new IllegalArgumentException("array for partition cannot be null");
+            throw new IllegalArgumentException("Table cannot be null.");
         }
         if (tableForPartition.length == 0) {
-            throw new IllegalArgumentException("array for partition length must be greater than zero");
+            throw new IllegalArgumentException("Table cannot be empty.");
         }
         if (pivotIndex < 0) {
-            throw new IllegalArgumentException("pivotIndex cannot be negative");
+            throw new IllegalArgumentException("Pivot index must be greater or equal 0.");
         }
         if (pivotIndex >= tableForPartition.length) {
-            throw new IllegalArgumentException("pivotIndex out of bound");
+            throw new IllegalArgumentException("Pivot index must be lower than table size.");
         }
         int[] partitionedTable = tableForPartition.clone();
         int leftCursor = 1;
         int rightCursor = partitionedTable.length - 1;
+        int pivotValue = partitionedTable[pivotIndex];
         swapElementsInArray(partitionedTable, 0, pivotIndex);
         while (leftCursor < rightCursor) {
-            while (partitionedTable[leftCursor] <= partitionedTable[0] && leftCursor != partitionedTable.length - 1) {
+            while (partitionedTable[leftCursor] <= pivotValue && leftCursor != partitionedTable.length - 1) {
                 leftCursor++;
             }
-            while (partitionedTable[rightCursor] > partitionedTable[0] && rightCursor != 0) {
+            while (partitionedTable[rightCursor] > pivotValue && rightCursor != 0) {
                 rightCursor--;
             }
             if (leftCursor < rightCursor) {
